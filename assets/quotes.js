a7dad2e4f81cb900a73fd6665ef39525318b036a
HFZ1928 <fzhanif929@gmail.com>
2020-05-04 05:01:45 +0700

    initial commit

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "haniffz", quotes: "Whatever happens, keep breathing" },
+    { author: "haniffz", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "haniffz", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "haniffz", quotes: "I love you." },
+    { author: "haniffz", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "haniffz", quotes: "I love you." },
+    { author: "haniffz", quotes: "I'm sorry for loving you." },
+];
